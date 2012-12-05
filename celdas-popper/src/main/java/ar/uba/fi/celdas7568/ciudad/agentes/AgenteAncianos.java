47b774db73f5de97f1b257d5671b96d28035b5c5
seba90s <seba90s@848e5917-5e45-74c2-4dc2-6cdce095c417>
2012-12-05 00:30:38 +0000

    Cambio en la forma de aprendizaje para tener en cuenta la importancia de cada característica.

--- celdas-popper/src/main/java/ar/uba/fi/celdas7568/ciudad/agentes/AgenteAncianos.java
+++ celdas-popper/src/main/java/ar/uba/fi/celdas7568/ciudad/agentes/AgenteAncianos.java
@@ -7,31 +7,31 @@ public class AgenteAncianos extends Agente{
 	
 		public AgenteAncianos(){
 		
-		barrioExclusivoDeseeado = 4;
+		barrioExclusivoDeseado = 4;
 		costoDeseado = 3;
-		culturaDeseeado = 5;
-		diversionDeseeado = 1;
-		educacionDeseeado = 1;
-		familiarDeseeado = 5;
-		naturalezaDeseeado = 4;
-		populosoDeseeado = 1;
-		seguridadDeseeado = 5;
-		tranquiloDeseeado = 4;
-		transporteDeseeado = 2;
+		culturaDeseado = 5;
+		diversionDeseado = 1;
+		educacionDeseado = 1;
+		familiarDeseado = 5;
+		naturalezaDeseado = 4;
+		populosoDeseado = 1;
+		seguridadDeseado = 5;
+		tranquiloDeseado = 4;
+		transporteDeseado = 2;
 		
 		importanciaBarrioExclusivo = 0.1;
-		importanciaCosto = 1;
-		importanciaCultura = 1;
+		importanciaCosto = 0.8;
+		importanciaCultura = 0.7;
 		importanciaDiversion = 0.2;
 		importanciaEducacion = 0;
-		importanciaFamiliar = 0.9;
+		importanciaFamiliar = 0.8;
 		importanciaNaturaleza = 0.75;
-		importanciaPopuloso = 1;
-		importanciaSeguridad = 1;
-		importanciaTranquilo = 1;
+		importanciaPopuloso = 0.8;
+		importanciaSeguridad = 0.8;
+		importanciaTranquilo = 0.9;
 		importanciaTransporte = 0.1;
 		
-		coeficienteDeAprendizaje = 0.005;
+		coeficienteDeAprendizaje = 0.02;
 	}
 
 }
--- celdas-popper/src/main/java/ar/uba/fi/celdas7568/ciudad/agentes/AgenteFamilia.java
+++ celdas-popper/src/main/java/ar/uba/fi/celdas7568/ciudad/agentes/AgenteFamilia.java
@@ -6,30 +6,30 @@ public class AgenteFamilia extends Agente{
 
 		public AgenteFamilia(){
 		
-		barrioExclusivoDeseeado = 2;
+		barrioExclusivoDeseado = 2;
 		costoDeseado = 3;
-		culturaDeseeado = 3;
-		diversionDeseeado = 3;
-		educacionDeseeado = 5;
-		familiarDeseeado = 5;
-		naturalezaDeseeado = 3;
-		populosoDeseeado = 1;
-		seguridadDeseeado = 4;
-		tranquiloDeseeado = 3;
-		transporteDeseeado = 1;
+		culturaDeseado = 3;
+		diversionDeseado = 3;
+		educacionDeseado = 5;
+		familiarDeseado = 5;
+		naturalezaDeseado = 3;
+		populosoDeseado = 1;
+		seguridadDeseado = 4;
+		tranquiloDeseado = 3;
+		transporteDeseado = 1;
 		
 		importanciaBarrioExclusivo = 0.7;
 		importanciaCosto = 0.2;
 		importanciaCultura = 0.2;
 		importanciaDiversion = 0.8;
-		importanciaEducacion = 1;
-		importanciaFamiliar = 1;
+		importanciaEducacion = 0.9;
+		importanciaFamiliar = 0.9;
 		importanciaNaturaleza = 0.6;
 		importanciaPopuloso = 0.8;
 		importanciaSeguridad = 0.75;
 		importanciaTranquilo = 0.5;
 		importanciaTransporte = 0.2;
 		
-		coeficienteDeAprendizaje = 0.005;
+		coeficienteDeAprendizaje = 0.02;
 		}
 }
